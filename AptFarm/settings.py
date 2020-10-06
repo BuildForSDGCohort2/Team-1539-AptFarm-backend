94d24660114e36b95a73f9170f0e5203816fb142
Uchehub <53799451+Uchehub@users.noreply.github.com>
2020-10-06 12:48:09 +0100

    make it better

--- AptFarm/settings.py
+++ AptFarm/settings.py
@@ -116,7 +116,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+import dj_database_url
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
